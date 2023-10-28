18a184e579279fc1319e8eafb6cf61818ca94b42
Marc Chouteau <marc_chouteau@hotmail.com>
2023-10-28 16:13:12 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LicenseUploaderWebApp.Pages;
+
+[DisableRequestSizeLimit]
+[IgnoreAntiforgeryToken]
+public class IndexModel : PageModel
+{
+	private readonly ILogger<IndexModel> _logger;
+	private readonly IConfiguration _configuration;
+
+	public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+	{
+		_logger = logger;
+		_configuration = configuration;
+	}
+
+	public async Task<IActionResult> OnPostUploadAsync(IFormFile file)
+	{
+		if (file == null
+			|| file.Length == 0)
+		{
+			return new OkResult();
+		}
+
+		_logger.LogInformation("Uploading file {FileName} ({Length} bytes)", file.FileName, file.Length);
+
+		var section = _configuration.GetSection("Upload");
+		var path = section.GetValue<string>("Path")!;
+
+		var fileName = Path.Combine(path, file.FileName);
+		var folder = Path.GetDirectoryName(fileName);
+		if (!Directory.Exists(folder))
+		{
+			Directory.CreateDirectory(folder!);
+		}
+
+		using (var stream = new FileStream(fileName, FileMode.Create))
+		{
+			await file.CopyToAsync(stream);
+		}
+
+		return new OkResult();
+	}
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿const camera = (() => {
+	const startCamera = async (videoSelector) => {
+
+		if (!navigator?.mediaDevices?.getUserMedia) {
+			console.error("getUserMedia is not supported");
+			return;
+		}
+
+		const constraints = {
+			video: {
+				width: 1280,
+				height: 720
+			},
+			audio: false
+		};
+		const stream = await navigator.mediaDevices.getUserMedia(constraints);
+		if (!stream) {
+			console.error("getUserMedia returned null");
+			return;
+		}
+
+		const videoElement = document.getElementById(videoSelector);
+		videoElement.srcObject = stream;
+		videoElement.play();
+	};
+
+	const stopCamera = (videoSelector) => {
+		const videoElement = document.getElementById(videoSelector);
+		if (videoElement) {
+			try {
+				console.log(videoElement);
+				videoElement.pause();
+				videoElement.srcObject.getTracks().forEach(track => track.stop());
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	};
+
+	const takePicture = (videoSelector, canvasSelector, dotNetHelper) => {
+		const videoElement = document.getElementById(videoSelector);
+		const canvasElement = document.getElementById(canvasSelector);
+
+		const context = canvasElement.getContext('2d');
+		context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+
+		const dataUrl = canvasElement.toDataURL('image/jpeg');
+		dotNetHelper.invokeMethodAsync("ProcessImage", dataUrl);
+	};
+
+	return {
+		startCamera,
+		stopCamera,
+		takePicture
+	};
+})();
+
+window.camera = window.camera || camera;
